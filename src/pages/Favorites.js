ecce75d37a950fc8b2bb27adc7001450a7bfdfdc
jongho <jj0101065@gmail.com>
2023-01-30 22:15:04 +0900

    즐겨찾기 기능 추가

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import FavoritesContext from '../store/favorites-context';
+import MeetupList from '../components/meetups/MeetupList';
 
 const Favorites = () => {
+  const favoritesCtx = useContext(FavoritesContext);
   return (
-    <div>
-      favorite
-    </div>
+    <section>
+      <h1>My Favorites</h1>
+      {favoritesCtx.totalFavorites === 0 ? (
+        <p>즐겨찾기가 없어요.</p>
+      ) : (
+        <MeetupList items={favoritesCtx.favorites} />
+      )}
+    </section>
   );
 };
 
